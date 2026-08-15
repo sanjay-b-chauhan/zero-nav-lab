839faa1a10a02520b579a9e1f7364a6c57af8d17
Sanjay Chauhan <social@z-ro.co>
2026-08-16 00:33:27 +0530

    burn meter on wall-clock elapsed time (throttle-proof)

--- assets/react-BTClSFwQ.js
+++
@@ -1,1 +0,0 @@
-import{V as o}from"./index-CwWxZxbq.js";const t=o;export{t as m};
--- assets/refreshAccountBilling-B5TrOWxa.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-CwWxZxbq.js","assets/index-DAtyTeix.css"])))=>i.map(i=>d[i]);
-import{_ as n}from"./index-CwWxZxbq.js";async function o(e="reset"){try{const{useUsageBillingStore:t}=await n(async()=>{const{useUsageBillingStore:i}=await import("./index-CwWxZxbq.js").then(a=>a.bg);return{useUsageBillingStore:i}},__vite__mapDeps([0,1]));await t.getState().invalidateAfterReset()}catch(t){console.warn(`[billing] Failed to invalidate account billing after ${e}:`,t)}}export{o as invalidateAccountBillingAfterReset};
