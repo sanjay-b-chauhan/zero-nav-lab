75ca441abc357d238280762c62bef0ae02ed307a
Sanjay Chauhan <social@z-ro.co>
2026-08-15 21:57:32 +0530

    earn simulator: click credits = celebration + odometer on credits AND xp pills; wordmark out of the scrim

---
+++ assets/react-D5NvAeil.js
@@ -0,0 +1,1 @@
+import{V as o}from"./index-DAhpaP5Z.js";const t=o;export{t as m};
--- assets/refreshAccountBilling-DIc81rc7.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-BzpPP4ZW.js","assets/index-BLQP14AH.css"])))=>i.map(i=>d[i]);
-import{_ as n}from"./index-BzpPP4ZW.js";async function o(e="reset"){try{const{useUsageBillingStore:t}=await n(async()=>{const{useUsageBillingStore:i}=await import("./index-BzpPP4ZW.js").then(a=>a.bg);return{useUsageBillingStore:i}},__vite__mapDeps([0,1]));await t.getState().invalidateAfterReset()}catch(t){console.warn(`[billing] Failed to invalidate account billing after ${e}:`,t)}}export{o as invalidateAccountBillingAfterReset};
