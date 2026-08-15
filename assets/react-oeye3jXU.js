3900067bae66a8e50323064a70419df7c45fce7b
Sanjay Chauhan <social@z-ro.co>
2026-08-15 21:36:23 +0530

    fix tether attach after card recentre; card gets est-time+difficulty; wordmark aligned to band-2 line

---
+++ assets/react-oeye3jXU.js
@@ -0,0 +1,1 @@
+import{V as o}from"./index-B5bq8iw2.js";const t=o;export{t as m};
--- assets/refreshAccountBilling-PwRKxLh-.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index-eWOcp87z.js","assets/index-CvBSwNO0.css"])))=>i.map(i=>d[i]);
-import{_ as n}from"./index-eWOcp87z.js";async function o(e="reset"){try{const{useUsageBillingStore:t}=await n(async()=>{const{useUsageBillingStore:i}=await import("./index-eWOcp87z.js").then(a=>a.bg);return{useUsageBillingStore:i}},__vite__mapDeps([0,1]));await t.getState().invalidateAfterReset()}catch(t){console.warn(`[billing] Failed to invalidate account billing after ${e}:`,t)}}export{o as invalidateAccountBillingAfterReset};
